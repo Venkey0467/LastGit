f97cebfb9d927c95113c7ef2f361e66a6b2a8120
Venkey0467 <venkataramana3094@gmail.com>
2023-06-08 06:31:24 -0700

    second commit

--- src/test/java/Library/AddUser.java
+++ src/test/java/Library/AddUser.java
@@ -3,14 +3,19 @@ package Library;
 import static io.restassured.RestAssured.*;
 import org.testng.annotations.*;
 
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
 public class AddUser {
+	 
+	int id;
 	
 	// Post Request
 	
 	@Test (priority = 1)
-	public void AddPlace()
+	public void CreateUser()
 	{
-		given()
+		Response res = given()
 		.contentType("application/json")
 		.body("{\r\n"
 				+ "    \"name\" : \"Boss\",\r\n"
@@ -20,6 +25,22 @@ public class AddUser {
 		.when()
 			.post("http://localhost:3000/posts/")
 		.then()	
-			.log().all();  
+			.log().all()
+			.extract().response();
+		JsonPath jp = new JsonPath(res.asString());
+		id = jp.get("id");
+	}
+	
+	// Get Request
+	
+	@Test (priority = 1)
+	public void GetUser()
+	{
+		given()
+			.pathParam("key" , id)
+		.when()
+			.get("http://localhost:3000/posts/{key}")
+		.then()	
+			.log().all();
 	}
 }
